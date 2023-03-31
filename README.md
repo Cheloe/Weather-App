e098c8809610a9ecd789a19bb161369a5820dd60
Cheryl L <cheryl.loeffler@gmail.com>
2023-03-30 21:35:04 -0600

    logic for 5-day forecast and displaying same

--- README.md
+++ README.md
@@ -7,8 +7,7 @@
 <li> Defined OpenWeather api search queries
 <li> Since the weather api requires lat/long coordinates, and I want users to be able to search by city name, I wrote a function to retrieve the lat/long coordinates of a given city from OpenWeather's coordinate finder api
 <li> Created function to enter those coords into a forecast query and extract from the response the forecast elements I want to display to users
-<li> 
-<li>
+<li> Wrote logic to retrieve daily forecast(rather than every 3 hours)from hourly forecast array using a method I learned on <a href='https://stackoverflow.com/a/33483070'>Stack Overflow</a>. Thanks <a href='https://stackoverflow.com/users/586181/nicholas'>@nicholas!</a>
 <li>
 <li>
 <li>
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,11 @@ var todaysContainerEl = document.getElementById('todays-forecast');
 var fiveDayContainerEl = document.getElementById('five-day-forecast');
 
 var apiKey = 'b7d4b53a4c12da243b0e55e69f915177';
-var weatherApiUrl = 'https://api.openweathermap.org'
+//var weatherApiUrl = 'https://api.openweathermap.org'
 
 
 
-function fetchWeather(lat, lon) {
+function fetchWeather(location, lat, lon) {
     var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
     fetch(weatherUrl)
@@ -20,25 +20,47 @@ function fetchWeather(lat, lon) {
         return response.json();
       })
       .then(function (data) {
-        var forecast = data
-        console.log(forecast.list[1].weather[0])
-        for (let i=0; i<5; i++) {
+        console.log(data.list);
+        //to use weather API icons, would have to choose index of 4 or 8 depending on if it's day or night?
+        var forecast = data.list.filter(function(value, index, Arr) {
+          return index % 8 == 0;
+        });
+        //console.log(forecast);
+        var city = location;
+        for (let i=0; i < forecast.length; i++) {
             // could just append the stuff right away rather than assigning variables ie 
             //descEl = document.createElement("p");
             //descEl.textContent = forecast.list[i].weather[0].description
-            var weather = forecast.list[i].weather[0].description;
-            var icon = forecast.list[i].weather[0].icon;
-            var temp = forecast.list[i].main.temp;
-            var humidity = forecast.list[i].main.humidity
-            var wind = forecast.list[i].wind.speed
-      }
-      console.log(weather);
-            console.log(icon);
-            console.log(temp + " degrees");
-            console.log(humidity + "%");
-            console.log(wind + " mph");
-        });      
+            // var weather = forecast.list[i].weather[0].description/img/wn/01n@2x.png;
+            var icon = forecast[i].weather[0].icon;
+            var temp = Math.trunc(forecast[i].main.temp);
+            var humidity = forecast[i].main.humidity
+            var wind = forecast[i].wind.speed
+        }
+       displayForecast(city, icon, temp, humidity, wind); 
+    });      
 };
+
+function displayForecast(city, icon, temp, humidity, wind){
+  var forecastCityEl = document.createElement('h2');
+  var iconEl = document.createElement('img');
+  var humidityEl = document.createElement('h5');
+  var windEl = document.createElement('h5');
+  var tempEl = document.createElement('h5');
+
+  var iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+  iconEl.setAttribute("src", iconUrl);
+  humidityEl.textContent = humidity + " %";
+  windEl.textContent= wind + " mph";
+  forecastCityEl.textContent = city;
+  tempEl.textContent = temp + " F";
+  
+  todaysContainerEl.append(forecastCityEl);
+  todaysContainerEl.append(tempEl);
+  todaysContainerEl.append(iconEl);
+  todaysContainerEl.append(humidityEl);
+  todaysContainerEl.append(windEl);
+}
 
 function fetchCoordinates(cityName) {
     // Insert the API url to get a list of your repos
@@ -52,17 +74,17 @@ function fetchCoordinates(cityName) {
       .then(function (data) {
         //looping over the fetch response and inserting the URL of your repos into a list
         console.log(data[0]);
-        var location = data[0];
-        var lat = location.lat;
-        var lon = location.lon;   
+        var location = data[0].name;
+        var lat = data[0].lat;
+        var lon = data[0].lon;   
         
-        fetchWeather(lat, lon);
+        fetchWeather(location, lat, lon);
       })
 }
 
 searchBtnEl.addEventListener('click', function(){
-    // var cityName = searchFieldEl.value.trim();
-    var cityName = 'Atlanta';
+    var cityName = searchFieldEl.value.trim();
+    //var cityName = 'Atlanta';
     console.log(cityName);
     fetchCoordinates(cityName)
 });
